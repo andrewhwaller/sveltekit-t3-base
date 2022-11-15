eeaee49c6471498eb4fd2a30ffa4466ff8383e7b
andrewhwaller <andrew@andrewhwaller.com>
2022-11-14 19:37:34 -0600

    Update class ordering and icons

--- src/lib/components/NavToggle.svelte
+++ src/lib/components/NavToggle.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { Icon } from '@steeze-ui/svelte-icon';
-	import { XMark} from '@steeze-ui/heroicons';
+	import { XMark } from '@steeze-ui/heroicons';
 	import { SidebarCollapse, SidebarExpand } from '@steeze-ui/octicons';
 
 	export let show: boolean;
@@ -12,24 +12,24 @@
 	<button
 		on:click={() => (minimal = !minimal)}
 		type="button"
-		class="my-auto text-gray-600 hover:text-gray-600 hover:bg-gray-200 dark:text-gray-400 flex items-center px-2 py-1.5 text-xs
-                      rounded-md dark:hover:bg-gray-800 dark:hover:text-white gap-1"
+		class="my-auto flex items-center gap-1 rounded-md px-2 py-1.5 text-xs text-gray-600 hover:bg-gray-200
+                      hover:text-gray-600 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white"
 	>
 		<Icon
 			src={minimal ? SidebarExpand : SidebarCollapse}
 			class=" h-3 w-3 {minimal && 'ml-0.5 my-0.5'} rotate-180"
 		/>
-		<span class="{minimal && 'hidden'} ml-2 my-auto">Minimize sidebar</span>
+		<span class="{minimal && 'hidden'} my-auto ml-2">Minimize sidebar</span>
 	</button>
 	{#if allowClose}
 		<button
 			on:click={() => (show = !show)}
 			type="button"
-			class="my-auto text-gray-600 hover:text-gray-600 hover:bg-gray-200 dark:text-gray-400 flex items-center px-2 py-1.5 text-xxs
-                        rounded-md dark:hover:bg-gray-800 dark:hover:text-white gap-2"
+			class="my-auto flex items-center gap-2 rounded-md px-2 py-1.5 text-xs text-gray-600 hover:bg-gray-200
+                        hover:text-gray-600 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white"
 		>
 			<Icon src={XMark} theme="solid" class="h-3 w-3 {minimal && 'ml-0.5 my-0.5'}" />
-			<span class="{minimal && 'hidden'} ml-1 my-auto">Close sidebar</span>
+			<span class="{minimal && 'hidden'} my-auto ml-1">Close sidebar</span>
 		</button>
 	{/if}
 </div>
