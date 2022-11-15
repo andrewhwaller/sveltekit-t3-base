d61542f8823e2c6a6198cd3f54bb8d558f98fe73
andrewhwaller <andrew@andrewhwaller.com>
2022-11-14 19:35:40 -0600

    Update formatting

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,26 +4,26 @@ import type { HTTPHeaders } from '@trpc/client';
 import { resolveHTTPResponse } from '@trpc/server/http';
 
 export const handle: Handle = async ({ event, resolve }) => {
-    if (event.url.pathname.startsWith("/api/trpc/")) {
-        const result = await resolveHTTPResponse({
-            createContext: async () => ({ event }),
-            path: event.url.pathname.substring("/api/trpc/".length),
-            req: {
-                body: event.request.body,
-                headers: <HTTPHeaders><unknown>event.request.headers,
-                method: event.request.method,
-                query: event.url.searchParams
-            },
-            router: appRouter
-        })
-        return new Response(
-            result.body,
-            {
-                headers: <HeadersInit><unknown>result.headers,
-                status: result.status
-            }
-        )
-    }
-    
-    return resolve(event)
+	if (event.url.pathname.startsWith("/api/trpc/")) {
+		const result = await resolveHTTPResponse({
+			createContext: async () => ({ event }),
+			path: event.url.pathname.substring("/api/trpc/".length),
+			req: {
+				body: event.request.body,
+				headers: <HTTPHeaders><unknown>event.request.headers,
+				method: event.request.method,
+				query: event.url.searchParams
+			},
+			router: appRouter
+		})
+		return new Response(
+			result.body,
+			{
+				headers: <HeadersInit><unknown>result.headers,
+				status: result.status
+			}
+		)
+	}
+
+	return resolve(event)
 }
