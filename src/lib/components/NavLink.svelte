0ea87e6622e26f05c378a7498c1cbdaea69e5487
andrewhwaller <andrew@andrewhwaller.com>
2022-11-14 19:36:40 -0600

    Update props

--- src/lib/components/NavLink.svelte
+++ src/lib/components/NavLink.svelte
@@ -1,24 +1,22 @@
 <script lang="ts">
-	import type { IconSource } from '@steeze-ui/svelte-icon/types';
 	import { Icon } from '@steeze-ui/svelte-icon';
-	import {page} from "$app/stores";
+	import { page } from '$app/stores';
+	import type { NavItem } from '$lib/config';
 
-	export let href: string;
-	export let icon: IconSource;
-	export let name: string;
-	export let minimal: boolean;
+	export let item: NavItem;
+	export let minimal: boolean = false;
 	export let navMode = 'sidebar';
 	export let thinNavbar = false;
 </script>
 
 <a
-	{href}
-	class:active={$page.url.pathname === href}
+	href={item.href}
+	class:active={$page.url.pathname === item.href}
 	class="my-auto {navMode === 'navbar' && thinNavbar ? '' : 'rounded-md'}
-  text-gray-600 dark:text-white flex items-center px-2 py-1.5 text-xs"
+  flex items-center px-2 py-1.5 text-xs text-gray-600 dark:text-white"
 >
-	{#if icon}
-		<Icon src={icon} theme="outline" class="text-blue-500 block h-4 w-4" />
+	{#if item.icon}
+		<Icon src={item.icon} theme="outline" class="block h-4 w-4 text-blue-500" />
 	{/if}
-	<span class="{minimal && 'hidden'} ml-2 whitespace-nowrap">{name}</span>
+	<span class="{minimal && 'hidden'} ml-2 whitespace-nowrap">{item.name}</span>
 </a>
