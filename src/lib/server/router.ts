71ee65114c3996bc20b8d9f088778aa80226f359
andrewhwaller <andrew@andrewhwaller.com>
2022-11-15 19:02:56 -0600

    Fix quotes

--- src/lib/server/router.ts
+++ src/lib/server/router.ts
@@ -1,8 +1,8 @@
-import { helloProcedure } from "../../routes/api/trpc/hello";
+import { helloProcedure } from '../../routes/api/trpc/hello';
 import { t } from './trpc';
 
 export const appRouter = t.router({
   hello: helloProcedure
-})
+});
 
 export type AppRouter = typeof appRouter;
